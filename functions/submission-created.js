114aabdfdbf276b45e811e97ae147762956c58b1
A Glasspool <rubygrade@gmail.com>
2021-02-18 12:13:11 +0100

    Update submission-created.js

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -1,6 +1,6 @@
 exports.handler = async function(event, context) {
     return {
         statusCode: 200,
-        body: JSON.stringify(context)
+        console.log(JSON.stringify(context))
     };
 }
