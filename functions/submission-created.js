3fbdfe25e86cbf8048941a6635e7dd44c5eac119
A Glasspool <rubygrade@gmail.com>
2021-02-18 13:29:57 +0100

    Update submission-created.js

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -1,6 +1,6 @@
 exports.handler = function(event, context, callback) {
   const data = JSON.parse(event.body)
-  console.log(data.name)
+  console.log(data)
   callback(null, {
     statusCode: 200,
     body: JSON.stringify(data.name),
